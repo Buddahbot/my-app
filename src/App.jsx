4abf6b373dcfb62026d3e63df24c8ec607932afc
martin <martin.lange@csa-germany.de>
2022-02-18 13:03:41 +0100

    edit readme, add my new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import { Typography } from '@material-ui/core';
+import AwesomeAppsFeatures from './components/awesome-apps-feature';
 
 
 
 function App() {
   return (
     <div>
-      <Typography variant="h1">Hello World</Typography>
+      <Typography variant="h2">Hello World</Typography>
+      <AwesomeAppsFeatures />
     </div>
   );
 }
---
+++ src/components/awesome-apps-feature.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AwesomeAppsFeatures = () => {
+    return (
+      <div>
+        Awesome apps features
+      </div>
+    );
+  }
+  
+  export default AwesomeAppsFeatures;
+  
