79c67136715e4fb9ea59f317a4b3e0a32dfcbf9a
Buddahbot <90890525+Buddahbot@users.noreply.github.com>
2022-02-18 11:11:04 +0100

    psd react template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import { Typography } from '@material-ui/core';
+
+
+
+function App() {
+  return (
+    <div>
+      <Typography variant="h1">Hello World</Typography>
+    </div>
+  );
+}
+
+export default App;
